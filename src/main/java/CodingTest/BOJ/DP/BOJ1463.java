bc6292dd7984b27fa5d736de7336c0e40f0fa248
JHyun0302 <wogustkfkd11@gmail.com>
2024-09-15 22:50:28 +0900

    :sparkles: [DP] 1로 만들기

---
+++ src/main/java/CodingTest/BOJ/DP/BOJ1463.java
@@ -0,0 +1,28 @@
+package CodingTest.BOJ.DP;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ1463 {
+    static final int MAX_N = 1000000;
+    static int n;
+    static int[] dp = new int[MAX_N + 1];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + 1;
+            if (i % 3 == 0) {
+                dp[i] = Math.min(dp[i / 3] + 1, dp[i]);
+            }
+            if (i % 2 == 0) {
+                dp[i] = Math.min(dp[i / 2] + 1, dp[i]);
+            }
+        }
+
+        System.out.println(dp[n]);
+    }
+}
